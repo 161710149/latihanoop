25ce7ad80cc87d7291bb64fecdc66acec450f970
rhidayat122 <151610243@smkassalaambandung.sch.id>
2017-10-24 08:31:46 +0400

    latihanoop

---
+++ index5.php
@@ -0,0 +1,13 @@
+<?php
+
+	class kambing{
+
+		public $jenis = 'mamalia';
+		public $bulu = 'keriting';
+
+	}
+	$kambing1 = new kambing;
+	echo "Kambing hewan jenis :".$kambing1-> jenis .'<br>' ;
+	echo "dan berbulu :".$kambing1-> bulu .'<br>' ;
+
+?>
